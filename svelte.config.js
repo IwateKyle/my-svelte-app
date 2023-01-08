3f78b6a7a162a40a70aaa03431c36761703eedf7
Kyle Manning <kyle@pop-os.localdomain>
2023-01-08 21:35:21 +0900

    reverted cloudflare adapter settings

--- svelte.config.js
+++ svelte.config.js
@@ -9,17 +9,7 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter(
-			{
-				pages: 'build',
-				assets: 'build',
-				fallback: null,
-				precompress: false
-			}
-		),
-		// prerender: {
-		// 	default: true
-		// }
+		adapter: adapter(),
 	}
 };
 
